a218f39ab5165a73f5e61179baa30b04d815a09d
Praveen Kumar <praveen.kumar8@walmart.com>
2022-04-08 12:22:06 +0530

    changes

---
+++ src/main/java/com/walmart/webhook/WebhookHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookHandlerApplication.class, args);
+	}
+
+}
